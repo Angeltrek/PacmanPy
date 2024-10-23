5d9e18a6d4cb8b3b3e387b5c88f8a4379a55a831
Angeltrek <angeltrek007@gmail.com>
2024-10-23 10:35:59 -0600

    feat: if applied, this commit will increase the ghosts number

--- pacman.py
+++ pacman.py
@@ -22,9 +22,12 @@ aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
     [vector(-180, 160), vector(5, 0)],
+    [vector(-180, 100), vector(5, 0)],
     [vector(-180, -160), vector(0, 5)],
     [vector(100, 160), vector(0, -5)],
+    [vector(100, 100), vector(0, -5)],
     [vector(100, -160), vector(-5, 0)],
+    [vector(100, -120), vector(-5, 0)],
 ]
 # fmt: off
 tiles = [
